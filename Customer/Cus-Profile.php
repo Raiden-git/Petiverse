327ae0a5bccd85b623a559fec1047b4bdecf070d
Prashid710 <prashiddilshan0710@gmail.com>
2024-09-12 23:48:36 +0530

    profile

--- Customer/Cus-Profile.php
+++ Customer/Cus-Profile.php
@@ -22,8 +22,7 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     $user = mysqli_fetch_assoc($result);
 } else {
-    echo "No user found!"; // Debugging line
-    exit();
+    echo "No user found!"; 
 }
 
 // Close the connection
